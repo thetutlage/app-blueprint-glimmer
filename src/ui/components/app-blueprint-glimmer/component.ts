2c163a3e51551da2a12289cc25f7e3eaa3579867
Richard Livsey <richard@livsey.org>
2017-08-29 16:45:45 +0100

    Add @kayako/app and setup connection

--- src/ui/components/app-blueprint-glimmer/component.ts
+++ src/ui/components/app-blueprint-glimmer/component.ts
@@ -1,5 +1,18 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+
+import KayakoApp from '@kayako/app';
 
 export default class AppBlueprintGlimmer extends Component {
+
+  @tracked ready: boolean = false;
+
+  constructor(options) {
+    super(options);
+
+    const app = new KayakoApp();
+    app.onReady(() => {
+      this.ready = true;
+    });
+  }
 
 }
